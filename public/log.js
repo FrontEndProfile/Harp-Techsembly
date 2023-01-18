3cd444463552d12e0e1bf291f25be86ffee59200
Fazal Ur Rehman <fazal.rehman@techsembly.com>
2023-01-18 17:12:02 +0500

    harp-tech_v_01

---
+++ public/log.js
@@ -0,0 +1,36 @@
+//  document.querySelectorAll('.short_card').style.color = "blue";
+// blog_pages.forEach(blog_page => {
+//     blog_page.style.backgroundColor = 'purple';
+//     blog_page.style.width = '300px';
+//   });
+// blog_pages
+// console.log(blog_pages);
+
+// const note = document.querySelector('.note');
+// note.style.backgroundColor = 'yellow';
+// note.style.color = 'red';
+
+
+
+
+// $( document ).ready(function() {
+//     // $( ".short_card" ).addClass( "myClass yourClass" );
+//     $(".short_card .btn-lgd").attr("href", "views/blog_page");
+
+//     // $( ".blog_page .blog_tittle" ).
+//     $(".blog_page .blog_tittle").replaceWith("#{ blog_01.title }");
+
+
+// });
+// import data from './harp.json' assert { type: 'JSON' };
+// import  data  from "/harp.json";
+// var express = require("express");
+// var harp = require("harp");
+// var app = express();
+
+// app.use(express.static(__dirname + "/public"));
+// app.use(harp.mount(__dirname + "/public"));
+
+// app.listen(9000);
+
+// console.log('ok 200');
